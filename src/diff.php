a2028d362c7dff3bbd7e564e4c068942f8ef08b1
M.Potapov <m.potapov@vsemayki.ru>
2018-12-04 12:57:26 +0700

    init project

---
+++ src/diff.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GenerateDiff;
+
+function run()
+{
+
+    $doc = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  --format <fmt>                Report format [default: pretty]
+DOC;
+
+    $args = (new \Docopt\Handler)->handle($doc);
+}
